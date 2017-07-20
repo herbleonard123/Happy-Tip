2efebdcbc84c9e85eca5b74f3ac990a99ecb34a1
StreetCode Academy <streetcodeacademy@SCA12-StreetCodes-MacBook-Air.local>
2017-07-19 20:31:56 -0700

    laball

--- Happy Tip/ViewController.swift
+++ Happy Tip/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipAmountLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+    view.endEditing(true)
+    }
+    @IBAction func Calculatetip(_ sender: UITextField) {
+    }
 }
 
